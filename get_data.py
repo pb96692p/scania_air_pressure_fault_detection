17554c7a40713454f244d5fcc540e5a0151db6bb
pb96692p <43291962+pb96692p@users.noreply.github.com>
2023-02-11 15:33:52 +0530

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -1,20 +1,15 @@
-<<<<<<< HEAD
+
 file_path="aps_failure_training_set1.csv"
-=======
-file_path="/config/workspace/aps_failure_training_set1.csv"
->>>>>>> origin/main
+
 from sensor.data_access.sensor_data import SensorData
 from sensor.constant.training_pipeline import DATA_INGESTION_COLLECTION_NAME
 from main import set_env_variable
 import os
 if __name__=='__main__':
-<<<<<<< HEAD
     data_file_path="aps_failure_training_set1.csv"
     env_file_path='env.yaml'
-=======
-    data_file_path="/config/workspace/aps_failure_training_set1.csv"
-    env_file_path='/config/workspace/env.yaml'
->>>>>>> origin/main
+    data_file_path="aps_failure_training_set1.csv"
+    env_file_path='env.yaml'
     set_env_variable(env_file_path)
     print( os.environ['MONGO_DB_URL'])
     sd = SensorData()
